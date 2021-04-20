eecd6eb789799436c623a4dc12bc61387685da24
Cristobal <cristobaljohn00@gmail.com>
2021-04-20 09:41:43 -0500

    basicosAdd

---
+++ 03_typescript_intro/src/ejercicios/basicos.ts
@@ -0,0 +1,15 @@
+
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+//las const no cambian de valor, let si
+let nombre = 'Strider';
+let hp: number | string = 95;
+let vivo: boolean = true;
+
+hp = 'infinito';
+
+console.log(nombre, hp, vivo);
+
