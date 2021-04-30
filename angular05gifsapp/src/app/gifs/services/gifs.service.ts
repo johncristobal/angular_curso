13d754e8f23ac2b329b9ec67e828f6f5a05355eb
John_Vera <johnalexiscristobaljimenez6@gmail.com>
2021-04-29 23:17:42 -0500

    interfaces

--- angular05gifsapp/src/app/gifs/services/gifs.service.ts
+++ angular05gifsapp/src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchsGifsResponse } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root' // este servicio sera uncio y global
@@ -9,14 +10,16 @@ export class GifsService {
   private _historial: string[] = [];
   private apikey = '0Guxt7sN00kUM97FB4uRrKqH8IAjyOdR';
 
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial(){
     return [...this._historial];
   }
 
   constructor(private http: HttpClient){
-
+    if(localStorage.getItem('historial')){
+      this._historial = JSON.parse( localStorage.getItem('historial')! ) || [];
+    }
   }
 
   buscarGifs(query: string = ''){
@@ -26,11 +29,13 @@ export class GifsService {
     if( !this._historial.includes(query) ){
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
-    this.http.get(
+    this.http.get<SearchsGifsResponse>(
       `https://api.giphy.com/v1/gifs/search?api_key=0Guxt7sN00kUM97FB4uRrKqH8IAjyOdR&q=${query}&limit=10`
-    ).subscribe( (resp:any) => {
+    ).subscribe( (resp: SearchsGifsResponse ) => {
       this.resultados = resp.data;
     });
   }
