1f0e2c0737298a62813e3fb6201592389048295f
Cristobal <cristobaljohn00@gmail.com>
2021-05-07 10:07:09 -0500

    finCurso

--- angular06paises/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ angular06paises/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
+import { switchMap, tap } from 'rxjs/operators';
+import { Paises } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -8,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  pais!: Paises;
+
+  constructor( private activeRoute: ActivatedRoute, private servive: PaisService) { }
 
   ngOnInit(): void {
-  }
 
+    this.activeRoute.params
+      .pipe(
+        switchMap( (params) => this.servive.buscarPaisPorId(params.id)),
+        tap( console.log )
+      )
+      .subscribe( resp => {
+        this.pais = resp;
+      });
+
+    // this.activeRoute.params
+    //   .subscribe( (params) => {
+    //     this.servive.buscarPaisPorId(params.id)
+    //       .subscribe( pais => {
+    //         console.log(pais);
+    //       });
+    //   })
+  }
 }
--- angular06paises/src/app/pais/services/pais.service.ts
+++ angular06paises/src/app/pais/services/pais.service.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { identifierName } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
@@ -25,5 +26,10 @@ export class PaisService {
   buscarCapital(termino: string): Observable<Paises[]>{
     const url = `${this.api_url}/capital/${termino}`;
     return this.http.get<Paises[]>( url );
+  }
+
+  buscarPaisPorId(id: string): Observable<Paises>{
+    const url = `${this.api_url}/alpha/${id}`;
+    return this.http.get<Paises>( url );
   }
 }
