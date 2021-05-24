f9b4ea554caa9054547a8e1ea77099d4e3c42cdf
John_Vera <johnalexiscristobaljimenez6@gmail.com>
2021-05-24 10:41:09 -0500

    formsSgtarted

---
+++ angular09forms/src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+interface MenuItem{
+  text: string;
+  ruta: string;
+}
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styles: [
+  ]
+})
+export class SidemenuComponent {
+
+  rutas: MenuItem[] = [
+    {
+      text: 'Basicos',
+      ruta: './template/basicos'
+    },
+    {
+      text: 'Dinamicos',
+      ruta: './template/dinamicos'
+    },
+    {
+      text: 'Switches',
+      ruta: './template/switches'
+    },
+  ]
+
+  rutasR: MenuItem[] = [
+    {
+      text: 'Basicos',
+      ruta: './reactive/basicos'
+    },
+    {
+      text: 'Dinamicos',
+      ruta: './reactive/dinamicos'
+    },
+    {
+      text: 'Switches',
+      ruta: './reactive/switches'
+    },
+  ]
+}
