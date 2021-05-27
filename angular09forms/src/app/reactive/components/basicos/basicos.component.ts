6a12e6ad16a32310dee944afd7507b7731b040f7
John_Vera <johnalexiscristobaljimenez6@gmail.com>
2021-05-26 21:34:24 -0500

    arraySet

--- angular09forms/src/app/reactive/components/basicos/basicos.component.ts
+++ angular09forms/src/app/reactive/components/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  // miForm: FormGroup = new FormGroup({
+  //   nombre: new FormControl('RTX Vas'),
+  //   precio: new FormControl(0),
+  //   existencias: new FormControl(0),
+  // });
+
+  miForm: FormGroup = this.builder.group({
+      nombre: [
+        , 
+        [
+           Validators.required,
+           Validators.minLength(3)
+        ]
+      ],
+      precio: [
+        , 
+        [
+          Validators.required,
+          Validators.min(0)
+        ]
+      ],
+      existencias: [
+        ,
+        [
+          Validators.required,
+          Validators.min(0)
+        ]
+      ],
+  });
+
+  constructor(private builder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miForm.reset({
+      nombre: 'spiderman',
+      precio: 1000,
+    })
   }
+
+  campoNoValido(campo: string){
+    return this.miForm.controls[campo].errors && this.miForm.controls[campo].touched;
+  }
+
+  guardar(){
+    if(this.miForm.invalid){
+      this.miForm.markAllAsTouched(); //se llaman todos como tocacos y con eso se ejecuta el errors
+      return;
+    }
+
+    this.miForm.reset();
+  }
+
 
 }
--- angular09forms/src/app/reactive/components/dinamicos/dinamicos.component.ts
+++ angular09forms/src/app/reactive/components/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -8,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+  miForm: FormGroup = this.builder.group({
+    nombre: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(3)
+      ]
+    ],
+    favoritos: this.builder.array([
+        ['Spiderman', Validators.required],
+        ['Venom', Validators.required]
+      ], Validators.required )
+  })
+
+  get favoritosArr(){
+    return this.miForm.get('favoritos') as FormArray;
+  }
+
+  constructor(private builder: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  campoNoValido(campo: string){
+    return this.miForm.controls[campo].errors && this.miForm.controls[campo].touched;
+  }
+
+  guardar(){
+    if(this.miForm.invalid){
+      this.miForm.markAllAsTouched(); //se llaman todos como tocacos y con eso se ejecuta el errors
+      return;
+    }
+
+    this.miForm.reset();
   }
 
 }
