5bf36fdaa87a23af7ef31a2716d2709870965665
John_Vera <johnalexiscristobaljimenez6@gmail.com>
2021-06-02 16:57:16 -0500

    regisgterSet

--- angular09forms/src/app/auth/pages/registro/registro.component.ts
+++ angular09forms/src/app/auth/pages/registro/registro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { ValidatorService } from 'src/app/shared/services/validator.service';
+import { EmailValidatorService } from 'src/app/shared/validator/email-validator.service';
 import { nombrePattern, emailPattern, noStrider } from 'src/app/shared/validator/validaciones';
 
 @Component({
@@ -14,13 +15,26 @@ export class RegistroComponent implements OnInit {
 
   miForm: FormGroup = this.builder.group({
     nombre: ['', [ Validators.required, Validators.pattern( this.validatorService.nombrePattern ) ] ],
-    email: ['', [ Validators.required, Validators.pattern( this.validatorService.emailPattern ) ] ],
+    email: ['', [ Validators.required, Validators.pattern( this.validatorService.emailPattern ) ], [ this.emailV ] ],
     username: ['', [ Validators.required, this.validatorService.noStrider ] ],
     password: ['', [ Validators.required, Validators.minLength(6) ] ],
     confirm: ['', [ Validators.required ] ],
   }, {
     validators: [ this.validatorService.camposIguales('password','confirm') ]
   });
+
+  get emailErrorMsg() : string {
+    const errors = this.miForm.get('email')?.errors;
+    if(errors?.required){
+      return 'Email obligatorio';
+    } else if (errors?.pattern){
+      return 'Formato invalido';
+    }else if (errors?.emailTomado){
+      return 'Email ya existe';
+    }
+
+    return '';
+  }
 
   campoNoValido(campo: string){
     return this.miForm.get(campo)?.invalid
@@ -33,7 +47,8 @@ export class RegistroComponent implements OnInit {
 
   constructor(
     private builder: FormBuilder,
-    private validatorService: ValidatorService
+    private validatorService: ValidatorService,
+    private emailV: EmailValidatorService
   ) { }
 
   ngOnInit(): void {
