952abd61a42fd328d902c5454a4b79e024f9a388
John_Vera <johnalexiscristobaljimenez6@gmail.com>
2021-06-09 07:36:19 -0500

    mapasSet

--- angular12mapas/src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ angular12mapas/src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
   styles: [
+    `
+    div{
+      width: 100%;
+      height: 150px;
+      margin: 0px;
+    }
+    `
   ]
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
+
+  @Input() lngLat: [number, number] = [0,0];
+  @ViewChild('mapa') divMapa!: ElementRef; 
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngAfterViewInit(){
+    const mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      //container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    });
 
+    new mapboxgl.Marker()
+    .setLngLat(this.lngLat)
+    .addTo(mapa);
+  }
 }
--- angular12mapas/src/app/mapas/pages/marcadores/marcadores.component.ts
+++ angular12mapas/src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,144 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+interface MarkColor {
+  color: string;
+  marker?: mapboxgl.Marker,
+  center?: [number, number]
+}
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
+    `
+    .mapa-container{
+      width: 100%;
+      height: 100%;
+    }
+    .list-group {
+      position: fixed;
+      top: 20px;
+      right: 20px;
+      z-index: 99;
+    }
+    li{
+      cursor: pointer;
+    }
+    `
   ]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+
+  mapa!: mapboxgl.Map;
+  @ViewChild('mapa') divMapa!: ElementRef;
+
+  // array marker
+  marcadores: MarkColor[] = [];
+
+  zoomLevel: number = 15;
+  center: [number, number] = [ -99.15493134593737, 19.412416616140234 ];
+
+  ngAfterViewInit(): void {    
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      //container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel
+    });
+
+    this.leerLocalData();
+    
+    // const markerH : HTMLElement = document.createElement('div');
+    // markerH.innerHTML = 'Hiii';
+
+    // const marker = new mapboxgl.Marker({
+    //   element: markerH
+    // })
+    // .setLngLat(this.center)
+    // .addTo(this.mapa);
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  irMarcador( marker: mapboxgl.Marker ){
+    this.mapa.flyTo({
+      center: marker.getLngLat(),
+      zoom: 17
+    });
   }
 
+  agregarM(){
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+
+    const mark = new mapboxgl.Marker({
+      draggable: true,
+      color: color
+    })
+      .setLngLat( this.center )
+      .addTo(this.mapa);
+
+    this.marcadores.push({
+      color: color,
+      marker: mark,
+      center: this.center
+    }); 
+
+    this.guardarMarcadores();
+
+    mark.on('dragend',()=> {
+      this.guardarMarcadores();
+    });
+  }
+
+  guardarMarcadores(){
+    const arr: MarkColor[] = [];
+
+    this.marcadores.forEach( m => {
+      const color = m.color;
+      const { lng, lat } = m.marker!.getLngLat();
+
+      arr.push({
+        color: color,
+        center: [ lng, lat ]
+      });
+    });
+
+    localStorage.setItem('marcadores', JSON.stringify(arr));
+  }
+
+  leerLocalData(){
+    if(!localStorage.getItem('marcadores')){
+      return;
+    }
+
+    const ar: MarkColor[] = JSON.parse( localStorage.getItem('marcadores')! );
+    ar.forEach( m => {
+
+      const mark = new mapboxgl.Marker({
+        color : m.color,
+        draggable: true
+      })
+      .setLngLat( m.center! )
+      .addTo( this.mapa );
+
+      this.marcadores.push({
+        marker: mark,
+        color: m.color
+      })
+
+      mark.on('dragend',()=> {
+        this.guardarMarcadores();
+      });
+    });
+  }
+
+
+  borrarM(i: number){
+    this.marcadores[i].marker?.remove();
+    this.marcadores.splice(i,1);
+
+    this.guardarMarcadores();
+  }
 }
